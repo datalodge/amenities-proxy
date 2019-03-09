924146031c4a64411f03313e1813ff3cc251488c
Victor Liu <victorliu716@gmail.com>
2019-03-08 19:28:34 -0800

    Deploy local proxy server with microservice

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 const express = require('express');
-//const bodyParser = require('body-parser');
+const proxy = require('http-proxy-middleware');
+const compression = require('compression')
+const cors = require('cors')
+const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors())
+app.use(compression())
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-app.use(express.static(__dirname + '/public'));
+
+app.use('/amenities', proxy({
+  target: 'http://localhost:3003',
+  changeOrigin: true
+}))
+
 
 app.listen(port, () => {
   console.log(`I'm serving from http://localhost:${port}`);
